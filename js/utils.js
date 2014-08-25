c370bbb448fe8c920a90b7aedb3226bb4f6293d7
tuxgasy <contact@tuxgasy.fr>
2014-08-25 18:20:19 +0200

    update hexToString()

--- js/utils.js
+++ js/utils.js
@@ -8,8 +8,8 @@ function stringToHex(str){
 
 function hexToString(hex){
   var str = '';
-  for(var i=0;i<str.length;i++){
+  for(var i=0;i<hex.length;i+=2){
     str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
   }
-  return hex;
+  return str;
 }
