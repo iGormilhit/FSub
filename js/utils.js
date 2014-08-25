1a0030fa6d84a5c0d9f90ffcb1722f2b2c3b48b7
tuxgasy <contact@tuxgasy.fr>
2014-08-25 17:58:43 +0200

    add hexToString function

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,15 @@
 function stringToHex(str){
-	var hex ='';
-    for(var i=0;i<str.length;i++){
-        hex += str.charCodeAt(i).toString(16);
-    }
-    
-    return hex;
+  var hex = '';
+  for(var i=0;i<str.length;i++){
+    hex += str.charCodeAt(i).toString(16);
+  }
+  return hex;
+}
+
+function hexToString(hex){
+  var str = '';
+  for(var i=0;i<str.length;i++){
+    str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+  }
+  return hex;
 }
